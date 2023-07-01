53c9bf4cf3eabc02dd070bdb80050cf4bcebe9a1
eddiew <eddieforwork0911@gmail.com>
2023-07-01 16:40:52 +1000

    toolyip

--- client/js/initialise.js
+++ client/js/initialise.js
@@ -2,6 +2,9 @@ import { renderHeader } from "./components/header.js";
 // import { renderChallengeList } from "./components/challengeList.js";
 import { renderHomePage } from "./components/home.js";
 
+const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
+const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
+
 renderHeader();
 // renderChallengeList();
 renderHomePage();
