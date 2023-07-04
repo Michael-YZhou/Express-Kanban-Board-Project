633257d925b255689d394a9ad49efb25c438e38c
Yang Zhou <yangzhouinter@gmail.com>
2023-07-04 19:57:03 +1000

    create mock data for presentation

--- controllers/boards.js
+++ controllers/boards.js
@@ -14,25 +14,207 @@ mongoClient
     db.dropCollection("boards");
     boardsCollection = db.collection("boards");
     boardsCollection.insertMany([
-      // board 1
       {
-        kanban_title: "Express Backend Project 1",
+        kanban_title: "Express Fullstack Project",
         kanban_creator: "Yang",
         kanban_members: ["Andreina", "Eddie"],
         kanban_desc: "Use Express to build a web server",
-        column_id: 2,
-        card_id: 3,
-        comment_id: 1,
-        // each board contains multiple columns
+        column_id: 5,
+        card_id: 14,
+        comment_id: 2,
         kanban_columns: [
           {
             column_id: 0,
             column_title: "Planning",
-            // each column contains multiple cards/tasks
             cards: [
               {
+                card_id: 2,
+                card_title: "Components Design",
+                card_desc: "code the component that renders the board.",
+                card_creator: "Yang",
+                card_members: ["Andreina", "Eddie"],
+                card_comment: [
+                  {
+                    comment_creator: "Yang",
+                    comment_create_time: "",
+                    comment_edit_time: "",
+                    comment_content: "This is one of the MVP features.",
+                  },
+                ],
+              },
+              {
+                card_id: 5,
+                card_title: "Sessions",
+                card_desc: "Planning  how user authentication validation work",
+                card_creator: "Yang",
+                card_members: ["Yang"],
+                card_comment: [
+                  {
+                    comment_creator: "Yang",
+                    comment_create_time: "04/07/2023, 19:02:26",
+                    comment_content: " created this card to Planning at",
+                  },
+                ],
+              },
+            ],
+          },
+          {
+            column_id: 2,
+            column_title: "Todo",
+            cards: [
+              {
+                card_id: 7,
+                card_title: "UI - Choose the color scheme",
+                card_desc: "choose the color scheme for the app",
+                card_creator: "Yang",
+                card_members: ["Yang"],
+                card_comment: [
+                  {
+                    comment_creator: "Yang",
+                    comment_create_time: "04/07/2023, 19:04:45",
+                    comment_content: " created this card to Todo at",
+                  },
+                ],
+              },
+              {
+                card_id: 8,
+                card_title: "UI - Design the app logo",
+                card_desc: "Add a more detailed description",
+                card_creator: "Yang",
+                card_members: ["Yang"],
+                card_comment: [
+                  {
+                    comment_creator: "Yang",
+                    comment_create_time: "04/07/2023, 19:05:09",
+                    comment_content: " created this card to Todo at",
+                  },
+                ],
+              },
+              {
+                card_id: 9,
+                card_title: "UI - Design the page layout",
+                card_desc: "Add a more detailed description",
+                card_creator: "Yang",
+                card_members: ["Yang"],
+                card_comment: [
+                  {
+                    comment_creator: "Yang",
+                    comment_create_time: "04/07/2023, 19:05:48",
+                    comment_content: " created this card to Todo at",
+                  },
+                ],
+              },
+              {
+                card_id: 10,
+                card_title: "API - create Board API",
+                card_desc: "For render, add, delete board",
+                card_creator: "Yang",
+                card_members: ["Yang"],
+                card_comment: [
+                  {
+                    comment_creator: "Yang",
+                    comment_create_time: "04/07/2023, 19:07:28",
+                    comment_content: " created this card to Todo at",
+                  },
+                ],
+              },
+            ],
+          },
+          {
+            column_id: 1,
+            column_title: "In-progress",
+            cards: [
+              {
+                card_id: 12,
+                card_title: "API - create Cards API",
+                card_desc: "Adding/moving/deleting cards",
+                card_creator: "Yang",
+                card_members: ["Yang"],
+                card_comment: [
+                  {
+                    comment_creator: "Yang",
+                    comment_create_time: "04/07/2023, 19:09:12",
+                    comment_content: " created this card to Todo at",
+                  },
+                ],
+              },
+              {
+                card_id: 11,
+                card_title: "API - create Columns API",
+                card_desc: "Adding, Moving, Deleting columns",
+                card_creator: "Yang",
+                card_members: ["Yang"],
+                card_comment: [
+                  {
+                    comment_creator: "Yang",
+                    comment_create_time: "04/07/2023, 19:08:46",
+                    comment_content: " created this card to Todo at",
+                  },
+                ],
+              },
+            ],
+          },
+          {
+            column_id: 3,
+            column_title: "Blocked",
+            cards: [
+              {
+                card_id: 13,
+                card_title: "DB - Create a sample DB",
+                card_desc: "Add a more detailed description",
+                card_creator: "Yang",
+                card_members: ["Yang"],
+                card_comment: [
+                  {
+                    comment_creator: "Yang",
+                    comment_create_time: "04/07/2023, 19:13:53",
+                    comment_content: " created this card to Todo at",
+                  },
+                ],
+              },
+            ],
+          },
+          {
+            column_id: 4,
+            column_title: "Done",
+            cards: [
+              {
+                card_id: 6,
+                card_title: "API - Create User Login API",
+                card_desc: "Add a more detailed description",
+                card_creator: "Yang",
+                card_members: ["Yang"],
+                card_comment: [
+                  {
+                    comment_creator: "Yang",
+                    comment_create_time: "04/07/2023, 19:03:11",
+                    comment_content: " created this card to Todo at",
+                  },
+                  {
+                    comment_id: 1,
+                    comment_creator: "Yang",
+                    comment_create_time: "04/07/2023, 19:06:04",
+                    comment_content: "",
+                  },
+                ],
+              },
+              {
+                card_id: 4,
+                card_title: "API Design",
+                card_desc: "design what apis we need and how they work",
+                card_creator: "Yang",
+                card_members: ["Yang"],
+                card_comment: [
+                  {
+                    comment_creator: "Yang",
+                    comment_create_time: "04/07/2023, 19:00:50",
+                    comment_content: " created this card to Planning at",
+                  },
+                ],
+              },
+              {
                 card_id: 0,
-                card_title: "UI design confirmation",
+                card_title: "UI Design",
                 card_desc: "Design the UI for all components.",
                 card_creator: "Yang",
                 card_members: ["Andreina", "Eddie"],
@@ -47,17 +229,105 @@ mongoClient
                 ],
               },
               {
-                card_id: 2,
-                card_title: "component/board",
-                card_desc: "code the component that renders the board.",
+                card_id: 3,
+                card_title: "Database Structure Design",
+                card_desc: "Design mongodb layout",
                 card_creator: "Yang",
-                card_members: ["Andreina", "Eddie"],
+                card_members: ["Yang"],
                 card_comment: [
                   {
                     comment_creator: "Yang",
-                    comment_create_time: "",
-                    comment_edit_time: "",
-                    comment_content: "This is one of the MVP features.",
+                    comment_create_time: "04/07/2023, 18:59:02",
+                    comment_content: " created this card to Planning at",
+                  },
+                ],
+              },
+            ],
+          },
+        ],
+        total_columns: 5,
+      },
+      {
+        kanban_title: "GA Project 3 Workflow",
+        kanban_creator: "Yang",
+        kanban_members: ["Yang"],
+        kanban_desc: "task management for proj 3",
+        column_id: 5,
+        card_id: 16,
+        comment_id: 0,
+        total_columns: 5,
+        kanban_columns: [
+          {
+            column_id: 0,
+            column_title: "Andreina",
+            cards: [
+              {
+                card_id: 0,
+                card_title: "Designing the Logo",
+                card_desc: "More detail about this card..",
+                card_creator: "Yang",
+                card_members: ["Yang"],
+                card_comment: [
+                  {
+                    comment_creator: "Yang",
+                    comment_create_time: "04/07/2023, 19:15:52",
+                    comment_content: "create this card as a default card at",
+                  },
+                ],
+              },
+              {
+                card_id: 1,
+                card_title: "Designing the color scheme",
+                card_desc: "Add a more detailed description",
+                card_creator: "Yang",
+                card_members: ["Yang"],
+                card_comment: [
+                  {
+                    comment_creator: "Yang",
+                    comment_create_time: "04/07/2023, 19:21:15",
+                    comment_content: " created this card to Andreina at",
+                  },
+                ],
+              },
+              {
+                card_id: 2,
+                card_title: "Create the profile page",
+                card_desc: "display all boards",
+                card_creator: "Yang",
+                card_members: ["Yang"],
+                card_comment: [
+                  {
+                    comment_creator: "Yang",
+                    comment_create_time: "04/07/2023, 19:21:51",
+                    comment_content: " created this card to Andreina at",
+                  },
+                ],
+              },
+              {
+                card_id: 3,
+                card_title: "Create session for login",
+                card_desc: "Add a more detailed description",
+                card_creator: "Yang",
+                card_members: ["Yang"],
+                card_comment: [
+                  {
+                    comment_creator: "Yang",
+                    comment_create_time: "04/07/2023, 19:22:18",
+                    comment_content: " created this card to Andreina at",
+                  },
+                ],
+              },
+              {
+                card_id: 4,
+                card_title: "Connent the nav bar btns",
+                card_desc: "Add a more detailed description",
+                card_creator: "Yang",
+                card_members: ["Yang"],
+                card_comment: [
+                  {
+                    comment_creator: "Yang",
+                    comment_create_time: "04/07/2023, 19:22:53",
+                    comment_content: " created this card to Andreina at",
                   },
                 ],
               },
@@ -65,30 +335,183 @@ mongoClient
           },
           {
             column_id: 1,
-            column_title: "In-progress",
-            // each column contains multiple cards/tasks
+            column_title: "Eddie",
             cards: [
               {
-                card_id: 1,
-                card_title: "component/board",
-                card_desc: "code the component that renders the board.",
+                card_id: 5,
+                card_title: "Create API for cards",
+                card_desc: "Add a more detailed description",
                 card_creator: "Yang",
-                card_members: ["Andreina", "Eddie"],
+                card_members: ["Yang"],
                 card_comment: [
                   {
                     comment_creator: "Yang",
-                    comment_create_time: "",
-                    comment_edit_time: "",
-                    comment_content: "This is one of the MVP features.",
+                    comment_create_time: "04/07/2023, 19:23:15",
+                    comment_content: " created this card to Eddie at",
+                  },
+                ],
+              },
+              {
+                card_id: 6,
+                card_title: "Create UI for card components",
+                card_desc: "Add a more detailed description",
+                card_creator: "Yang",
+                card_members: ["Yang"],
+                card_comment: [
+                  {
+                    comment_creator: "Yang",
+                    comment_create_time: "04/07/2023, 19:23:31",
+                    comment_content: " created this card to Eddie at",
+                  },
+                ],
+              },
+              {
+                card_id: 7,
+                card_title: "Create card info page",
+                card_desc: "Add a more detailed description",
+                card_creator: "Yang",
+                card_members: ["Yang"],
+                card_comment: [
+                  {
+                    comment_creator: "Yang",
+                    comment_create_time: "04/07/2023, 19:24:10",
+                    comment_content: " created this card to Eddie at",
+                  },
+                ],
+              },
+              {
+                card_id: 8,
+                card_title: "Create card comment feature",
+                card_desc: "Add a more detailed description",
+                card_creator: "Yang",
+                card_members: ["Yang"],
+                card_comment: [
+                  {
+                    comment_creator: "Yang",
+                    comment_create_time: "04/07/2023, 19:24:49",
+                    comment_content: " created this card to Eddie at",
+                  },
+                ],
+              },
+            ],
+          },
+          {
+            column_id: 2,
+            column_title: "Yang",
+            cards: [
+              {
+                card_id: 9,
+                card_title: "Create the columns APIs",
+                card_desc: "Add a more detailed description",
+                card_creator: "Yang",
+                card_members: ["Yang"],
+                card_comment: [
+                  {
+                    comment_creator: "Yang",
+                    comment_create_time: "04/07/2023, 19:25:11",
+                    comment_content: " created this card to Yang at",
+                  },
+                ],
+              },
+              {
+                card_id: 10,
+                card_title: "Create UI for the board page",
+                card_desc: "Add a more detailed description",
+                card_creator: "Yang",
+                card_members: ["Yang"],
+                card_comment: [
+                  {
+                    comment_creator: "Yang",
+                    comment_create_time: "04/07/2023, 19:30:34",
+                    comment_content: " created this card to Yang at",
+                  },
+                ],
+              },
+              {
+                card_id: 11,
+                card_title: "Create sample DB",
+                card_desc: "Add a more detailed description",
+                card_creator: "Yang",
+                card_members: ["Yang"],
+                card_comment: [
+                  {
+                    comment_creator: "Yang",
+                    comment_create_time: "04/07/2023, 19:31:42",
+                    comment_content: " created this card to Yang at",
+                  },
+                ],
+              },
+            ],
+          },
+          {
+            column_id: 3,
+            column_title: "Elon Musk",
+            cards: [
+              {
+                card_id: 12,
+                card_title: "Fitness training for cage fight",
+                card_desc: "Add a more detailed description",
+                card_creator: "Yang",
+                card_members: ["Yang"],
+                card_comment: [
+                  {
+                    comment_creator: "Yang",
+                    comment_create_time: "04/07/2023, 19:32:07",
+                    comment_content: " created this card to Elon Musk at",
+                  },
+                ],
+              },
+              {
+                card_id: 13,
+                card_title: "Jodo fundamentals",
+                card_desc: "Add a more detailed description",
+                card_creator: "Yang",
+                card_members: ["Yang"],
+                card_comment: [
+                  {
+                    comment_creator: "Yang",
+                    comment_create_time: "04/07/2023, 19:32:38",
+                    comment_content: " created this card to Elon Musk at",
+                  },
+                ],
+              },
+              {
+                card_id: 15,
+                card_title: "Post on Twitter daily",
+                card_desc: "Add a more detailed description",
+                card_creator: "Yang",
+                card_members: ["Yang"],
+                card_comment: [
+                  {
+                    comment_creator: "Yang",
+                    comment_create_time: "04/07/2023, 19:34:02",
+                    comment_content: " created this card to Mark Zuckerberg at",
+                  },
+                ],
+              },
+            ],
+          },
+          {
+            column_id: 4,
+            column_title: "Mark Zuckerberg",
+            cards: [
+              {
+                card_id: 14,
+                card_title: "Just relax",
+                card_desc: "Add a more detailed description",
+                card_creator: "Yang",
+                card_members: ["Yang"],
+                card_comment: [
+                  {
+                    comment_creator: "Yang",
+                    comment_create_time: "04/07/2023, 19:33:13",
+                    comment_content: " created this card to Mark Zuckerberg at",
                   },
                 ],
               },
             ],
           },
         ],
-        get total_columns() {
-          return this.kanban_columns.length;
-        },
       },
     ]);
   })
